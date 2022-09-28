e667d38d1a2455bb1aeebb2fa1457119e1158258
Lucas Melnik <lucas_melnik.99@gmail.com>
2022-09-27 22:30:29 -0300

    adding web project to repo

---
+++ web/src/pages/EditContact/index.js
@@ -0,0 +1,88 @@
+import { useState, useEffect, useRef } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+import ContactForm from '../../components/ContactForm';
+import PageHeader from '../../components/PageHeader';
+import Loader from '../../components/Loader';
+import toast from '../../utils/toast';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+
+import ContactsService from '../../services/ContactsService';
+
+export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(
+          id,
+        );
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValue(contact);
+          setIsLoading(false);
+          setContactName(contact.name);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado!',
+          });
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history, safeAsyncAction]);
+
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const contactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(contactData.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato',
+      });
+    }
+  }
+
+  return (
+    <>
+      <Loader isLoading={isLoading} />
+
+      <PageHeader
+        title={isLoading ? 'Carregando...' : `Editar ${contactName}`}
+      />
+
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
+    </>
+  );
+}
